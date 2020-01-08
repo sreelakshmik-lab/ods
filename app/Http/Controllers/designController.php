819e7666e6cf0820412ff2be4314db3b602ff9cb
sreelakshmik-lab <sreelakshmicdas1996@gmail.com>
2020-01-07 17:51:09 -0800

    synatx correction

--- app/Http/Controllers/designController.php
+++ app/Http/Controllers/designController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\userReg;
-use App\DesignerProfile
+use App\DesignerProfile;
 use DB;
 
 class designController extends Controller
