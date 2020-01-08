41485e1fe1131a4dc482ea6f24e68b3193029bce
sreelakshmik-lab <sreelakshmicdas1996@gmail.com>
2020-01-07 17:30:38 -0800

    foreign key correction

--- database/migrations/2019_12_20_210742_create_userreg_table.php
+++ database/migrations/2019_12_20_210742_create_userreg_table.php
@@ -14,7 +14,7 @@ class CreateUserregTable extends Migration
     public function up()
     {
         Schema::create('userreg', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('firstname');
             $table->string('lastname');
             $table->string('gender');
