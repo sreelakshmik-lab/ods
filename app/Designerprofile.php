da4643be8ae0c0f022e263a007c8a80d78f23baf
sreelakshmik-lab <sreelakshmicdas1996@gmail.com>
2020-01-07 17:20:03 -0800

    table update

--- app/Designerprofile.php
+++ app/Designerprofile.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Designerprofile extends Model
 {
-    
-    protected $table = 'profiledesigner';
-    protected $fillable = ['fk_loginid','designerid','edu_quali','work_exp'];
+	protected $table = 'profiledesigner';
 }
